050df6bf18b27511e3d7935daf2a7323824f11fa
Priya Upkare <upkare.priya@gmail.com>
2024-10-03 15:50:22 +0530

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Office.Interop.Word;
+using Spire.Pdf;
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Application wordApp = new Application();
+            //Document wordDocument = wordApp.Documents.Open(@"D:\PriyaUpkare_Immediate_Joiner.docx");
+
+            //wordDocument.ExportAsFixedFormat(@"E:\documentsnew.pdf", WdExportFormat.wdExportFormatPDF);
+
+            //wordDocument.Close();
+            //wordApp.Quit();
+
+
+
+            //convert pdf to word
+            convertpdf();
+        }
+    static    void convertpdf() {
+
+            PdfDocument pdfDocument = new PdfDocument();
+            pdfDocument.LoadFromFile(@"E:\documentsnew.pdf");
+
+            pdfDocument.SaveToFile(@"E:\path123.docx", FileFormat.DOCX);
+        }
+    }
+}
